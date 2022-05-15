6d0a461fbe24f729201e43462f9fc4e749e578d4
zhengyanbin <zhengyanbin@bytedance.com>
2022-05-16 00:07:07 +0800

    feat: 移动端使用 refresh_token, 懒得重新点登录

--- service/login.tsx
+++ service/login.tsx
@@ -8,7 +8,7 @@ export const login = async ({ username, password }) => {
     const res: any = await postFetch(`/auth/login`, params);
     if (res) {
         const data = await res.json();
-        localStorage.setItem("token", data.access_token);
+        localStorage.setItem("token", data.refresh_token);
         localStorage.setItem("username", username);
         return true;        
     } else {
