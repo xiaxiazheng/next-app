262697f1186d19a454a29279891645927013eb40
zyb <zhengyanbin@bytedance.com>
2024-10-14 21:59:36 +0800

    style: optimize style

--- components/common/drawer-wrapper/index.tsx
+++ components/common/drawer-wrapper/index.tsx
@@ -1,24 +1,39 @@
+import useTouchBottomToTop from "../../../hooks/useTouchBottomToTop";
 import styles from "./index.module.scss";
 import { Drawer, DrawerProps } from "antd";
 
 const DrawerWrapper: React.FC<DrawerProps> = (props) => {
-    const { title, open, onClose, placement = "bottom", footer, height = '75vh', className, ...rest } = props;
+    const { title, open, onClose, placement = "bottom", footer, height = "75vh", className, ...rest } = props;
+
+    const tips = useTouchBottomToTop(
+        {
+            spanY: 200,
+            onChange: () => {
+                open && onClose(null);
+            },
+            isReverse: true,
+        },
+        [open]
+    );
 
     return (
-        <Drawer
-            open={open}
-            placement={placement}
-            className={`${styles.drawer} ${className}`}
-            onClose={onClose}
-            footer={footer}
-            title={title}
-            closeIcon={null}
-            closable={false}
-            height={height}
-            {...rest}
-        >
-            <div className={styles.content}>{props.children}</div>
-        </Drawer>
+        <>
+            <Drawer
+                open={open}
+                placement={placement}
+                className={`${styles.drawer} ${className}`}
+                onClose={onClose}
+                footer={footer}
+                title={title}
+                closeIcon={null}
+                closable={false}
+                height={height}
+                {...rest}
+            >
+                <div className={styles.content}>{props.children}</div>
+            </Drawer>
+            {tips}
+        </>
     );
 };
 
