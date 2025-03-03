7a6a1fc4f59b85188dd7cbe9fb8e5cf0cfd0966c
zhengyanbin <zhengyanbin@bytedance.com>
2025-03-03 11:47:06 +0800

    fix: fix settings type

--- hooks/useSettings.tsx
+++ hooks/useSettings.tsx
@@ -1,10 +1,22 @@
 import { useEffect, useState } from "react";
 import { getSettings } from "../service/settings";
 
+interface ContextType {
+    todoNameMap: Record<string, any>;
+    todoDescriptionMap: Record<string, any>;
+    todoPoolDefaultShow: number;
+    todoColorNameMap: Record<string, any>;
+    todoColorMap: Record<string, any>;
+    todoCategoryDefaultShow: number;
+    todoDefaultColor: number;
+    quickDecisionConfig: Record<string, any>;
+    todoShowBeforeToday: Record<string, any>;
+};
+
 let lastSettings = {};
 let isRequesting = false;
 const useSettings = () => {
-    const [settings, setSettings] = useState<any>({});
+    const [settings, setSettings] = useState<Partial<ContextType>>({});
 
     const getSettingsData = async () => {
         isRequesting = true;
--- hooks/useTouchEvent.tsx
+++ hooks/useTouchEvent.tsx
@@ -1,13 +1,13 @@
 import { useEffect } from "react";
 import TouchEventClass from "../utils/touch-event";
-import useSettings from "./useSettings";
+// import useSettings from "./useSettings";
 
 let touchEvent: TouchEventClass;
 let isInit = false;
 
 const useTouchEvent = () => {
-    const settings = useSettings();
-    const touchSafeXY = settings?.touchSafeXY; // 获取 settings 配置
+    // const settings = useSettings();
+    // const touchSafeXY = settings?.touchSafeXY; // 获取 settings 配置
 
     if (!touchEvent) {
         // 创建实例，用全局变量 touchEvent 保证只会有一个实例
@@ -22,11 +22,11 @@ const useTouchEvent = () => {
         }
     }, []);
 
-    useEffect(() => {
-        if (touchSafeXY) {
-            touchEvent.setSafeXY(touchSafeXY); // 将 settings 的配置写入 class 实例
-        }
-    }, [touchSafeXY]);
+    // useEffect(() => {
+    //     if (touchSafeXY) {
+    //         touchEvent.setSafeXY(touchSafeXY); // 将 settings 的配置写入 class 实例
+    //     }
+    // }, [touchSafeXY]);
 
     return touchEvent; // 返回唯一的实例给所有使用的地方
 };
